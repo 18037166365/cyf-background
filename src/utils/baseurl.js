fe68dc5c39f40c8ef3eb04e570a2715bbb2a84f5
peien <peien@iyenei.com>
2019-03-04 21:59:45 +0800

    baseurl

--- src/utils/baseurl.js
+++ src/utils/baseurl.js
@@ -4,7 +4,8 @@ switch (process.env.NODE_ENV) {
         baseUrl = "http://127.0.0.1:3009/"  //这里是本地的请求url
         break
     case 'production':
-        baseUrl = "http://cyf.nlxiaoniao.cn/"   //生产环境url
+        // baseUrl = "http://cyf.nlxiaoniao.cn/"   //生产环境url
+        baseUrl = " http://47.94.110.229:3009/"   //生产环境url
         break
 }
 
