b33aa545ac14535d7455243121813e7dc5fcca13
peien <peien@iyenei.com>
2019-03-18 16:27:51 +0800

    package.json

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   // 基本路径
   // baseUrl: process.env.NODE_ENV==='production'? '/admin': '/'
-  baseUrl: process.env.NODE_ENV==='production'? '/': '/'
+  publicPath: process.env.NODE_ENV==='production'? '/': '/'
 }
